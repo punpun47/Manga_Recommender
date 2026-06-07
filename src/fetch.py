9d1999f1bfe3f4022f05a6b60118c445a940290f
punpun47 <boardman.caleb@gmail.com>
2026-06-07 14:24:59 +1000

    Added fetch.py: pulls 250 manga from AniList API and saves to data/manga.json

---
+++ src/fetch.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+url = "https://graphql.anilist.co"
+
+manga_list = []
+
+# loop because anilist caps requests at 50 manga
+# doubled {, } to avoid syntax errors with .format()
+for i in range(1, 6):
+    query = """
+    query {{
+    Page(page: {0}, perPage: 50) {{
+        media(type: MANGA, sort: POPULARITY_DESC) {{
+        id
+        title {{
+            romaji
+            native }}
+        genres
+        averageScore
+        popularity
+        }}
+    }}
+    }}
+    """.format(i)
+
+    # Get nested dictionary from anilist
+    response = requests.post(url, json = {"query": query})
+    
+    # Add each mangas data onto list individually
+    manga_list.extend(response.json()["data"]["Page"]["media"])
+
+
+
+with open("../data/manga.json", "w") as file:
+    json.dump(manga_list, file, indent = 2)
